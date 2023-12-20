a15b8ef1a72965973963afb9391f0991bb15c548
Putu Andi <92725749+putuandi@users.noreply.github.com>
2023-12-20 20:56:13 +0800

    Update db.py

--- config/db.py
+++ config/db.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine, MetaData
-# engine=create_engine('mysql+mysqlconnector://root@localhost:3306/artiquest_database')
-engine=create_engine('mysql+mysqlconnector://root:12345678@34.101.61.129:3306/artiquest_database')
+# engine=create_engine('')
+engine=create_engine('')
 meta=MetaData()
 con=engine.connect()
